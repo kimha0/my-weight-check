e66009de74950533f65662a21314354cb8fd03a7
kimha0 <hyoreuno@naver.com>
2020-06-05 14:07:03 +0900

    fix: detail select box 연도별로 나오게 수정

--- src/routes/detail.svelte
+++ src/routes/detail.svelte
@@ -2,30 +2,24 @@
   import { onMount } from "svelte";
   import { fly } from 'svelte/transition';
   import { heightZero } from '../animations';
+  import { getYear, getMonth } from 'date-fns'
   import "tui-chart/dist/tui-chart.css";
+
+  const nowDate = new Date();
 
   let tui;
   let chartContainer;
 
   let scoops = 1;
-  let selectYear = 2020;
-  let selectMonth = 1;
+  let selectYear = getYear(nowDate);
+  let selectMonth = getMonth(nowDate);
+
+
+  const years = new Array(selectYear - 2019).fill(null).map((_, index) => 2020 + index);
+  const months = new Array(12).fill(null).map((_, index) => index + 1);
 
   let data = {
-    categories: [
-      "1월",
-      "2월",
-      "3월",
-      "4월",
-      "5월",
-      "6월",
-      "7월",
-      "8월",
-      "9월",
-      "10월",
-      "11월",
-      "12월"
-    ],
+    categories: months,
     series: [
       {
         name: "Hayoung",
@@ -34,7 +28,7 @@
     ]
   };
 
-  let options = {
+  const initalizeOptions = {
     theme: "customTheme",
     chart: {
       width: 1160,
@@ -46,7 +40,13 @@
       areaOpacity: 0.8
     },
     tooltip: {
-      suffix: "Kg"
+      suffix: 'Kg'
+    },
+    xAxis: {
+      suffix: '월'
+    },
+    yAxis: {
+      suffix: 'kg'
     },
     legend: { visible: false },
     chartExportMenu: { visible: false }
@@ -88,7 +88,8 @@
     const { default: Chart } = await import("tui-chart");
     tui = Chart;
     tui.registerTheme("customTheme", theme);
-    tui.areaChart(chartContainer, data, options);
+    const i = new tui.areaChart(chartContainer, data, initalizeOptions);
+    i.update();
   });
 </script>
 
@@ -107,19 +108,17 @@
   <aside class="absolute bg-white rounded-md">
     <div class='m-3'>
       <select bind:value={selectYear} class='border border-gray-400 rounded-md px-2 py-2 w-full'>
-        <!-- TODO -->
-        <option value={2018}>2018</option>
-        <option value={2019}>2019</option>
-        <option value={2020}>2020</option>
+        {#each years as year (year)}
+          <option value={year} selected={year === selectYear}>{year}</option>
+        {/each}
       </select>
     </div>
     {#if scoops === 1}
     <div class='m-3' out:heightZero="{{ delay: 150, duration: 300 }}" in:heightZero="{{ delay: 0, duration: 300 }}">
       <select out:fly="{{ x: -200, duration: 300 }}" in:fly="{{ x: -200, duration: 300, delay: 150 }}" bind:value={selectMonth} class='border border-gray-400 rounded-md px-2 py-2 w-full'>
-        <!-- TODO -->
-        <option value={1}>1</option>
-        <option value={2}>2</option>
-        <option value={3}>3</option>
+        {#each months as month (month)}
+          <option value={month} selected={month === selectMonth}>{month}</option>
+        {/each}
       </select>
     </div>
     {/if}
